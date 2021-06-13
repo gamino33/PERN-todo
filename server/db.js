4bffca080804541500b02440c7bf9b94ee0144d0
gamino33 <standard3310@gmail.com>
2021-06-14 03:59:05 +0900

    add ssl

--- server/db.js
+++ server/db.js
@@ -8,7 +8,8 @@ const pool = new pg.Pool({
     password: process.env.ENV_PASSWORD,
     host: process.env.ENV_HOST,
     port: process.env.ENV_PORT,
-    database: process.env.ENV_DATABASE
+    database: process.env.ENV_DATABASE,
+    ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
