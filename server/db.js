023376626e9e2884353a9dc0a797482615d7b1d7
gamino33 <standard3310@gmail.com>
2021-06-14 01:05:04 +0900

    環境変数を使うように変更

--- server/db.js
+++ server/db.js
@@ -1,16 +1,14 @@
-import * as DB from "./dbKey.js";
-// import * as pg from 'pg'
-// const { Pool } = pg;
-
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new pg.Pool({
-    user: DB.USER,
-    password: DB.PASSWORD,
-    host: DB.HOST,
-    port: DB.PORT,
-    database: DB.DATABASE
+    user: process.env.ENV_USER,
+    password: process.env.ENV_PASSWORD,
+    host: process.env.ENV_HOST,
+    port: process.env.ENV_PORT,
+    database: process.env.ENV_DATABASE
 });
 
 export default pool;
-// module.exports = pool;
