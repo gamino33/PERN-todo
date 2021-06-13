87851be27645f5823b38e645f38802c0289a745c
gamino33 <standard3310@gmail.com>
2021-06-14 02:14:51 +0900

    add create

--- server/index.js
+++ server/index.js
@@ -9,7 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//create todo
+app.post("/todos", async(req, res) => {
+    try {
+        const description = req.body.description;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", [description]);
+        console.log(newTodo);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on ${ PORT }`);
-    console.log(process.env);
 });
